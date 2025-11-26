518a6907b842120d02ac733304732f1de4a17cba
Euwan Abogadie <yoowan28@gmail.com>
2025-11-26 13:32:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function toggleTheme() {
     document.body.classList.toggle("dark-mode");
 }
+
+// Smooth scroll for navigation links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
